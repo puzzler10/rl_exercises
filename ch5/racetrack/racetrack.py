82d06fcc6f39ba906615d7ec449a53133248f3cb
Tom <tom.roth@live.com.au>
2018-09-09 20:17:35 +1000

    racetrack update

--- ch5/racetrack/racetrack.py
+++ ch5/racetrack/racetrack.py
@@ -5,12 +5,17 @@ from collections import namedtuple
 from itertools import product
 import matplotlib.pyplot as plt
 import seaborn as sns
+from IPython.core.debugger import set_trace
+from math import log, e
+#%matplotlib auto
 
 #%%
 path = '/Users/tomroth/Dropbox/Reinforcement Learning/reinforcement_learning_exercises/ch5/racetrack/'
 
 track1 = pd.read_csv(path + 'racetrack1.csv', header=None)
 track2 = pd.read_csv(path + 'racetrack2.csv', header=None)
+
+
 
 def get_landmarks(track):
     """Extract start, finish and boundary coordinates for a track"""
@@ -26,69 +31,219 @@ def get_landmarks(track):
     Coords = namedtuple('Coords', 'start finish boundary')
     return Coords(start_coords, finish_coords, boundary_coords)
 
+def get_bounds_dict(bnds):
+    bounds_dict = dict()
+    for row in range(track.shape[0]):
+        bounds_dict[row] = [o.col for o in bnds if o.row == row]
+    return bounds_dict
+
+
+def get_finish_dict(bnds):
+    finish_dict = dict()
+    for o in bnds:
+        finish_dict[o.row] = o.col
+    return finish_dict
+
 def pick_random_start_location(x):
     """Given a list of start line coordinates, pick one at random
     x: list of tuples"""
     idx = floor(np.random.uniform(0, len(x)))
     return x[idx]
 
-def check_boundary_hit(loc, bnds):
-    """return True if loc would hit the boundary, false otherwise
-    bnds: boundaries"""
-    col_locs = [o.col for o in bnds if o.row == loc.row]
-    min_col, max_col =  min(col_locs), max(col_locs)
+def hit_landmark(loc, finish=False):
+    """return True if loc would hit the landmark (finish or boundaries),
+    false otherwise
+    finish: True if we're looking for finish line, false if looking for boundaries"""
+    # this is where we hit the top row
+    if loc.row <= 0:
+        if finish:      return False
+        else          : return True
+    # this case - we're not up to finish territory yet
     # In this case the car has hit the side walls
-    if loc.col <= min_col or loc.col >= max_col:     return True
-    # Else, we could still hit the boundary on the top row,
-    # or on a bit that juts out
-    for col in col_locs:
-        if loc.col == col:          return True
+    if finish:
+        if loc.col >= FINISH_DICT[loc.row]:        return True
+        if loc.row > max(FINISH_DICT.keys()):      return False
+    if not finish:
+        min_col, max_col = min(BOUNDS_DICT[loc.row]), max(BOUNDS_DICT[loc.row])
+        # one boundary case: it's om the left
+        if min_col == max_col:
+            if loc.col <= min_col:          return True
+        # multiple boundary case
+        else:
+            if loc.col <= min_col or loc.col >= max_col:     return True
+            # deal with boundary that juts out into racetrack
+            for col in BOUNDS_DICT[loc.row]:
+                if loc.col == col:                           return True
     return False
 
 
-def change_velocity(vel, dvel, p=0.1):
+def change_velocity(vel, dvel, loc, start_locs, p=0.1):
     """Velocity must be:
             - nonnegative at all times
             - not (0,0), except at the start line
-            - have each component between 0 and 4 inclusive
+            - have each component between 0 and -4 inclusive
+            - row is negative numbers because we're moving up the board
+            - column is positive because we're moving right
         With probability p dvel= 0, regardless of what was passed into dvel
     vel: current velocity
-    dvel: change in velocity"""
-    Velocity = namedtuple('Velocity', 'rowChange colChange')
-    if np.random.uniform < p:
+    dvel: change in velocity
+    loc: current_location"""
+    if np.random.uniform() < p:
         dvel = (0,0)
-    new_vel = tuple(np.add((1,2), (3,4)))
+    new_vel = tuple(np.add(vel, dvel))
+    if new_vel[0] > 0: new_vel = tuple((0, new_vel[1]))
+    if new_vel[1] < 0: new_vel = tuple((new_vel[0], 0))
+    if new_vel[0] < -4: new_vel = tuple((-4, new_vel[1]))
+    if new_vel[1] > 4: new_vel = tuple((new_vel[0], 4))
+    if new_vel == (0,0):
+        if not is_velocity_valid(new_vel, loc, start_locs):
+            # Invalid move: disregard it.
+            new_vel = vel
+    return new_vel
 
+
+def is_velocity_valid(vel, loc, start_locs):
+    """We can only have a 0,0 velocity if we are at the start line"""
+    if vel == (0,0) and loc not in start_locs:    return False
+    else:                                         return True
+
+
+def initialise_dicts(track,actions):
+    """Initialise q, C and the intial policy `pi`"""
+    q,C,pi = dict(),dict(),dict()
+    for i in range(track.shape[0]):
+        for j in range(track.shape[1]):
+            if track.iloc[i,j] in ['0', 'S'] :
+                for a in range(len(actions)):
+                    q[i,j,a] = np.random.uniform(0,1)
+                    C[i,j,a] = 0
+                max_act = np.argmax([q[o] for o in q if o[0] == i and o[1] == j])
+                pi[i,j] = actions[max_act]
+    return (q,C,pi)
+
+def generate_episode(b, actions, lm):
+    """Generate sequences S,A,R through simulating episode of car driving task"""
+    S,A,R = [],[],[0]
+    loc = pick_random_start_location(lm.start)
+    vel = (0,0)
+    # start position
+    ep_end = False
+    while not ep_end:
+        p_actions = [b[loc.row,loc.col, i] for i in range(len(actions))]
+        dvel = actions[np.argmax(np.cumsum(p_actions) > np.random.uniform())]
+        vel = change_velocity(vel, dvel, loc, lm.start)
+        S.append(loc)
+        A.append(dvel)
+        next_loc = tuple(np.add(vel, loc))
+        next_loc = Loc(next_loc[0], next_loc[1])
+        #print(loc, vel, next_loc, hit_landmark(next_loc, lm.boundary))
+        if hit_landmark(next_loc):
+            loc = pick_random_start_location(lm.start)
+            R.append(-1)
+            vel = (0,0)
+        elif hit_landmark(next_loc, finish=True):
+            R.append(0)
+            ep_end=True
+        else:
+            R.append(-1)
+            loc = next_loc
+    return (S,A,R)
+
+def update_policy(q, C, S, A, R, pi, b, gamma):
+    G = 0
+    W = 1
+    T = len(R) - 2
+    for t in range(T,-1,-1):
+        s,r = S[t],R[t+1]
+        a = [i for i,o in enumerate(actions) if o == A[t]][0]
+        # discounted previous return + next reward
+        G = gamma * G  + r
+        # cumulative weight sum for each state
+        # W is always above 1
+        C[s.row, s.col, a] +=  W
+        q[s.row, s.col, a] +=  (W / C[s.row, s.col, a]) * (G - q[s.row, s.col, a])
+        action = np.argmax([q[s.row, s.col, i] for i in range(len(actions))])
+        pi[s.row, s.col] = action
+        if a != action:
+            break
+        W *= (1 / b[s.row, s.col, action])
+    return (C,q,pi)
+
+def update_b(epsilon, b,q, states):
+    for s in states:
+        action = np.argmax([q[s[0], s[1], i] for i in range(len(actions))])
+        for a in range(len(actions)):
+            if a != action:
+                b[s[0], s[1], a] = epsilon / len(actions)
+            else:
+                b[s[0], s[1], a] = 1 - ((len(actions)-1) * epsilon / len(actions))
+    return b
+
+def off_policy_MC_control(track):
+    track = track1
+    lm = get_landmarks(track)
+
+
+def update_b(epsilon, b,q, states):
+    for s in states:
+        action = np.argmax([q[s[0], s[1], i] for i in range(len(actions))])
+        for a in range(len(actions)):
+            if a != action:
+                b[s[0], s[1], a] = epsilon / len(actions)
+            else:
+                b[s[0], s[1], a] = 1 - ((len(actions)-1) * epsilon / len(actions))
+    return b
 
 
 #%%
 # lm = landmarks
-Velocity = namedtuple('Velocity', 'rowChange colChange')
 Loc = namedtuple('Loc', 'row col')
-actions = list(product([-1,0,1], repeat = 2))
 #rewards =
-vel = Velocity(0,0)
-lm = get_landmarks(track1)
+vel = (0,0)
 
+track = track2
+lm = get_landmarks(track)
+BOUNDS_DICT = get_bounds_dict(lm.boundary)
+FINISH_DICT = get_finish_dict(lm.finish)
 
-loc = pick_random_start_location(lm.start)
+actions = list(product([-1,0,1], repeat = 2))
+q,C,pi = initialise_dicts(track, actions)
 
+#### setup
+# Init b as equiprobable random policy
+b = dict()
+for key in q.keys():
+    b[key] = 1/len(actions)
+states = set([(o[0],o[1]) for o in b.keys()])
 
+gamma = 0.9
+for i in range(300):
+    print(i)
+    S,A,R = generate_episode(b, actions, lm)
+    C,q,pi = update_policy(q, C, S, A, R, pi, b, gamma)
+    #if i < e:         epsilon = 1
+    #else:              epsilon = 1 / log(i,e)
+    #epsilon=1
+    #b = update_b(epsilon, b, q, states)
 
+v = dict()
+for s in states:
+    v[s[0],s[1]] = np.max([q[s[0], s[1], i] for i in range(len(actions))])
 
+#%%
+rows = [o[0] for o in v.keys()]
+cols = [o[1] for o in v.keys()]
+min_row, max_row = min(rows), max(rows)
+min_col, max_col = min(cols), max(cols)
+v_arr = np.zeros((max_row+1,max_col+1))
+for row in range(min_row, max_row+1):
+    for col in range(min_col, max_col+1):
+        try:
+            v_arr[row,col] = v[row,col]
+        except:
+            v_arr[row,col] = np.nan
 
-
-
-
-
-
-
-
-
-
-
-
-
+sns.heatmap(v_arr)
 
 
 
--- ch5/racetrack/racetrack_test.py
+++ ch5/racetrack/racetrack_test.py
@@ -1,5 +1,14 @@
 import pytest
 from racetrack import *
+import numpy as np
+
+Loc = namedtuple('Loc', 'row col')
+bnds = [Loc(4,2), Loc(4,7), Loc(3,2), Loc(3,3), Loc(3,4), Loc(3,8)]
+start_locs = [Loc(8,2), Loc(8,3), Loc(8,4)]
+finish_locs = [Loc(2,9), Loc(3,9), Loc(4,9)]
+track = pd.read_csv(path + 'racetrack1.csv', header=None)
+BOUNDS_DICT = get_bounds_dict(bnds)
+FINISH_DICT = get_finish_dict(finish_locs)
 
 def test_get_landmarks():
     landmarks = get_landmarks(track1)
@@ -8,19 +17,59 @@ def test_get_landmarks():
     assert (4,26) in landmarks.finish
     assert (1,26) in landmarks.finish
 
+def test_pick_random_start_location():
+    np.random.seed(1000)
+    start_loc_set = set()
+    for i in range(1000):
+        x = pick_random_start_location(start_locs)
+        assert x in start_locs
+        start_loc_set.add(x)
+    assert len(start_loc_set) == len(start_locs)
 
-def test_boundary_hit():
-    Loc = namedtuple('Loc', 'row col')
-    bnds = [Loc(4,2), Loc(4,7), Loc(3,2), Loc(3,3), Loc(3,4), Loc(3,8)]
-    assert check_boundary_hit(Loc(4,1), bnds) == True
-    assert check_boundary_hit(Loc(4,2), bnds) == True
-    assert check_boundary_hit(Loc(4,3), bnds) == False
-    assert check_boundary_hit(Loc(4,7), bnds) == True
-    assert check_boundary_hit(Loc(4,8), bnds) == True
-    assert check_boundary_hit(Loc(3,1), bnds) == True
-    assert check_boundary_hit(Loc(3,2), bnds) == True
-    assert check_boundary_hit(Loc(3,4), bnds) == True
-    assert check_boundary_hit(Loc(3,5), bnds) == False
-    assert check_boundary_hit(Loc(3,7), bnds) == False
-    assert check_boundary_hit(Loc(3,8), bnds) == True
-    assert check_boundary_hit(Loc(3,9), bnds) == True
+
+def test_landmark_hit():
+    assert hit_landmark(Loc(4,1)) == True
+    assert hit_landmark(Loc(4,2)) == True
+    assert hit_landmark(Loc(4,3)) == False
+    assert hit_landmark(Loc(4,7)) == True
+    assert hit_landmark(Loc(4,8)) == True
+    assert hit_landmark(Loc(3,1)) == True
+    assert hit_landmark(Loc(3,2)) == True
+    assert hit_landmark(Loc(3,4)) == True
+    assert hit_landmark(Loc(3,5)) == False
+    assert hit_landmark(Loc(3,7)) == False
+    assert hit_landmark(Loc(3,8)) == True
+    assert hit_landmark(Loc(3,9)) == True
+    assert hit_landmark(Loc(2,9) ,finish=True) == True
+    assert hit_landmark(Loc(2,10),finish=True) == True
+    assert hit_landmark(Loc(2,8),finish=True) == False
+
+
+def test_change_velocity():
+    loc = Loc(8,4)
+    assert change_velocity((0,2), (0,1) ,loc,start_locs, p=0) == (0,3)
+    assert change_velocity((0,1), (0,-1),loc,start_locs, p=0) == (0,0)
+    assert change_velocity((0,0), (0,-1),loc,start_locs, p=0) == (0,0)
+    assert change_velocity((0,0), (1,-1),loc,start_locs, p=0) == (0,0)
+    assert change_velocity((0,0), (-1,1),loc,start_locs, p=0) == (-1,1)
+    assert change_velocity((-4,4), (-1,1),loc,start_locs, p=0) == (-4,4)
+    assert change_velocity((0,1), (0,-1),Loc(8,2),start_locs, p=0) == (0,0)
+    assert change_velocity((0,1), (0,-1),Loc(9,2),start_locs, p=0) == (0,1)
+    assert change_velocity((-3,2), (-1,1),loc,start_locs, p=1) == (-3,2)
+
+
+def test_is_velocity_valid():
+    assert is_velocity_valid((0,0), Loc(8,2), start_locs) == True
+    assert is_velocity_valid((0,0), Loc(7,2), start_locs) == False
+    assert is_velocity_valid((-1,0), Loc(8,2), start_locs) == True
+    assert is_velocity_valid((-1,0), Loc(7,2), start_locs) == True
+
+
+def test_initialise_dicts():
+    actions = list(product([-1,0,1], repeat = 2))
+    q,C,pi = initialise_dicts(track, actions)
+    assert len(q)/len(actions) == len(pi)
+    assert sum(C.values()) == 0
+    max_q_arg = np.argmax([q[o] for o in q if o[0] == 8 and o[1] == 2])
+    assert pi[8,2] == actions[max_q_arg]
+
